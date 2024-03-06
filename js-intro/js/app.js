53d368ae178b21efa94aec43836b972056a72871
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-03-06 09:17:43 +0100

    ADD: js variabili

---
+++ js-intro/js/app.js
@@ -0,0 +1,46 @@
+console.log('Ciao dal file app.js')
+
+// let nome = 'Gianluca' // string
+// console.log(nome)
+
+// const age = 33 // number
+// console.log(age)
+// // age = 21 //non consentito
+
+// // let numeroA = 8
+// // let numeroB = 10
+
+// // const somma = numeroA + numeroB
+// // console.log(numeroA + numeroB) // number
+// // console.log(numeroA - numeroB) // number
+// // console.log(numeroA * numeroB) // number
+// // console.log(numeroA / numeroB) // number
+
+// // numeroA = 'ciao' // string
+
+// // console.log('somma: ', numeroA + numeroB) // string
+// // console.log('sottrazione: ', numeroA - numeroB)
+// // console.log('moltiplicazione: ', numeroA * numeroB)
+// // console.log('divisione: ', numeroA / numeroB)
+
+// // let userName = prompt('Inserisci il tuo nome') // string | null
+// // console.log('Ciao ' + userName)
+
+// // const userAge = prompt('Quanti anni hai?') // string | null
+// // console.log('Hai la bellezza di: ' + userAge + ' anni.')
+
+// let numero1 = prompt('Inserisci un numero') // string | null
+// numero1 = parseInt(numero1) // number
+
+// console.log(typeof numero1, numero1)
+// // console.log(typeof convertedNum1, convertedNum1)
+
+// const numero2 = parseInt(prompt('Inserisdci un altro numero')) // number
+// console.log(typeof numero2)
+
+// const somma = numero1 + numero2 // number
+// console.log(somma)
+
+const randomFloat = Math.random()
+console.log(randomFloat)
+// console.log(Math.PI)
